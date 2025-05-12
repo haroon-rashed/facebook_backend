da80411c0150fb70e1aaeff90a114882ae2f01c2
hamza rasheed <haronworkspace@gmail.com>
2025-05-13 03:03:03 +0500

    post done

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,18 +1,19 @@
 import { Post } from "../Models/postModel.js";
 
 export const addPost = async (req, res) => {
-  const { caption, background } = req.body;
+  const { caption, background, uploadImage } = req.body;
   const { user_id } = req.params;
 
-  if (!caption) {
-    return res.status(400).json({ message: "Please add a caption" });
-  }
+  // if (!caption) {
+  //   return res.status(400).json({ message: "Please add a caption" });
+  // }
 
   try {
     const newPost = await Post.create({
       caption,
       background,
       user_id,
+      uploadImage
     });
 
     res.status(201).json(newPost);
@@ -24,6 +25,34 @@ export const addPost = async (req, res) => {
 
 
 export const getPosts = async (req, res) =>{
-  const allPosts = await Post.find();
+  const allPosts = await Post.find().sort({createdAt : -1});
   res.send(allPosts)
 }
+
+export const makeReaction = async (req, res) => {
+  const { type } = req.body; 
+  const { post_id, user_id } = req.params;
+
+  try {
+    const findPost = await Post.findById(post_id);
+
+    if (!findPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const userReactionIndex = findPost.likes.findIndex((item) => item.id == user_id);
+
+    if (userReactionIndex === -1) {
+      findPost.likes.push({ type, id: user_id });
+    } else {
+      findPost.likes[userReactionIndex].type = type;
+    }
+
+    await findPost.save();
+
+    res.status(200).json(findPost);
+  } catch (error) {
+    console.error("Error making reaction:", error);
+    res.status(500).json({ message: "Server error while making reaction" });
+  }
+};
--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addPost, getPosts } from '../Controllers/postController.js';
+import { addPost, getPosts, makeReaction } from '../Controllers/postController.js';
 
 const postRouter = express.Router();
 
 
 postRouter.post('/add_post/:user_id', addPost);
 postRouter.get('/get-all-posts', getPosts);
+postRouter.post('/add-reaction/:post_id/:user_id', makeReaction)
 
 export default postRouter;
